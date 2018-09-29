3c2e5c3e89a07589dd97aaa503211d5f61189c1c
Kbrother <00076289@kaist.ac.kr>
2018-09-29 19:04:12 +0900

    0929 daytime

--- app/src/main/java/com/example/cs408_app/AlarmFragment.java
+++ app/src/main/java/com/example/cs408_app/AlarmFragment.java
@@ -1,11 +1,25 @@
 package com.example.cs408_app;
 
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.SyncStateContract;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+
+import com.google.android.gms.common.internal.Constants;
+import com.google.android.gms.location.Geofence;
+import com.google.android.gms.location.GeofencingClient;
+import com.google.android.gms.location.GeofencingRequest;
+import com.google.android.gms.location.LocationServices;
+
+import java.util.ArrayList;
+
+import static com.google.android.gms.location.Geofence.NEVER_EXPIRE;
 
 /**
  * Created by 권태형 on 2018-09-29.
@@ -17,6 +31,9 @@ public class AlarmFragment extends Fragment {
      * fragment.
      */
     private static final String ARG_SECTION_NUMBER = "section_number";
+    private GeofencingClient mGeofencingclient;
+    private ArrayList<Geofence> mGeofenceList;
+    private PendingIntent mGeofencePendingIntent;
 
     public AlarmFragment() {
     }
@@ -33,12 +50,63 @@ public class AlarmFragment extends Fragment {
         return fragment;
     }
 
+    private GeofencingRequest getGeofencingRequest() {
+        GeofencingRequest.Builder builder = new GeofencingRequest.Builder();
+        builder.setInitialTrigger(GeofencingRequest.INITIAL_TRIGGER_ENTER);
+        builder.addGeofences(mGeofenceList);
+        return builder.build();
+    }
+
+    private PendingIntent getGeofencePendingIntent() {
+        // Reuse the PendingIntent if we already have it.
+        if (mGeofencePendingIntent != null) {
+            return mGeofencePendingIntent;
+        }
+        Intent intent = new Intent(this, GeofenceTransitionsIntentService.class);
+        // We use FLAG_UPDATE_CURRENT so that we get the same pending intent back when
+        // calling addGeofences() and removeGeofences().
+        mGeofencePendingIntent = PendingIntent.getService(this, 0, intent, PendingIntent.
+                FLAG_UPDATE_CURRENT);
+        return mGeofencePendingIntent;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+
+        double latitude = 36.32139, longitude = 127.4192;
+        float radius = 100.0f;
+        String key = "dummy";
+
+        super.onCreate(savedInstanceState);
+        Context c = getActivity();
+        mGeofencingclient = LocationServices.getGeofencingClient(c);
+
+        mGeofenceList.add(new Geofence.Builder()
+                // Set the request ID of the geofence. This is a string to identify this
+                // geofence.
+                .setRequestId(key)
+
+                .setCircularRegion(
+                        latitude,
+                        longitude,
+                        radius
+                )
+                .setExpirationDuration(NEVER_EXPIRE)
+                .setTransitionTypes(Geofence.GEOFENCE_TRANSITION_ENTER |
+                        Geofence.GEOFENCE_TRANSITION_EXIT)
+                .build());
+
+
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_alarm, container, false);
         TextView textView = (TextView) rootView.findViewById(R.id.section_label);
         textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
+
+
         return rootView;
     }
 }
