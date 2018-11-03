7f7f9ebd7578ba7beffebeb5227e197b55379ca6
eunhyoukshin <silvershine157@gmail.com>
2018-11-03 16:01:21 +0900

    confirm, get input

--- app/src/main/java/com/example/cs408_app/SendReportActivity.java
+++ app/src/main/java/com/example/cs408_app/SendReportActivity.java
@@ -7,16 +7,25 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Toast;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class SendReportActivity extends AppCompatActivity {
     Button report_btn;
     EditText titleText;
+    EditText descText;
+    RadioGroup category;
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_send_report);
         report_btn = findViewById(R.id.button_report);
         titleText = (EditText) findViewById(R.id.title_input);
+        category = (RadioGroup) findViewById(R.id.category);
+        descText = (EditText) findViewById(R.id.desc_input);
         report_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -27,7 +36,15 @@ public class SendReportActivity extends AppCompatActivity {
                     @Override
                     public void onClick(DialogInterface dialogInterface, int i) {
                         String title = titleText.getText().toString();
-                        Toast.makeText(SendReportActivity.this, title, Toast.LENGTH_SHORT).show();
+                        List<String> supplierNames = Arrays.asList("opt1", "opt2", "opt3", "opt4", "opt5");
+                        int cat_id = category.getCheckedRadioButtonId();
+                        RadioButton r = (RadioButton) category.findViewById(cat_id);
+                        String cat_str = r.getText().toString();
+                        String desc = descText.getText().toString();
+
+                        // TODO: SEND REPORT
+                        Toast.makeText(SendReportActivity.this, "title: "+title+"\ncategory: "+cat_str+"\n", Toast.LENGTH_SHORT).show();
+
                         dialogInterface.dismiss();
                     }
                 });
