618c1db3a53a95ef212f8d3842acc9de028fa201
eunhyoukshin <silvershine157@gmail.com>
2018-11-05 13:49:35 +0900

    sending additional report data

--- app/src/main/java/com/example/cs408_app/Config/Constants.java
+++ app/src/main/java/com/example/cs408_app/Config/Constants.java
@@ -3,4 +3,6 @@ package com.example.cs408_app.Config;
 public class Constants {
 
     public static final String server_ip = "121.152.195.120";
+    public static final String server_port = ":8001";
+
 }
