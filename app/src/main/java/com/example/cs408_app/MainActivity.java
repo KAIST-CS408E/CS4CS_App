6faa31b6e8f2eeb112a6a49970cff9684a4bc548
조인영 (In-Young Cho) <ciy405x@kaist.ac.kr>
2018-11-15 03:43:18 +0900

    notification action button

--- app/src/main/java/com/example/cs408_app/MainActivity.java
+++ app/src/main/java/com/example/cs408_app/MainActivity.java
@@ -1,11 +1,13 @@
 package com.example.cs408_app;
 
+import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.support.v4.app.NotificationCompat;
@@ -15,6 +17,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.R.drawable;
 
 import com.example.cs408_app.Config.Constants;
 import com.example.cs408_app.MyFirebaseMessagingService.*;
@@ -128,22 +131,36 @@ public class MainActivity extends AppCompatActivity {
     /**
      * Test code for push alarm design
      */
+
+    /**
+     * setFullScreenIntent, registerIntent
+     *
+     * : launch the registerIntent(for example) instead of posting the notification to the status bar.
+     * Only for use with extremely high-priority notifications demanding the user's immediate attention,
+     * such as an incoming phone call or alarm clock that the user has explicitly set to a particular time.
+     */
     private void sendNotification(Map<String, String> dataMap){
 
         Intent intent = new Intent(this, MainActivity.class);
+        Intent registerIntent = new Intent(MainActivity.this, RegisterActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, registerIntent,
                 PendingIntent.FLAG_CANCEL_CURRENT);
 
         Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
         NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this, "MY_channel")
-                .setLargeIcon(BitmapFactory.decodeResource(getResources(), android.R.drawable.ic_dialog_info))
+                .setVisibility(Notification.VISIBILITY_PUBLIC)
+                //.addAction(R.drawable.ic_launcher_foreground, "Pause", pendingIntent)
+                .setLargeIcon(BitmapFactory.decodeResource(getResources(), android.R.drawable.ic_dialog_info)) // acquire an external resource by using URI(Uniform Resource Identifier)
                 .setSmallIcon(R.mipmap.ic_launcher)
                 .setContentTitle(dataMap.get("title"))
                 .setContentText(dataMap.get("content"))
                 .setAutoCancel(true)
                 .setSound(defaultSoundUri)
-                .setContentIntent(pendingIntent);
+                //.setVibrate(new long[] {1000,1000,1000 })
+                .addAction(drawable.ic_menu_view, "REGISTER", pendingIntent)
+                .addAction(drawable.ic_delete, "DELETE", null);
+                //.setFullScreenIntent(pendingIntent, true);
 
         NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
         notificationManager.notify(0 /* ID of notification */, notificationBuilder.build());
