ec6d06a6d3757f1206d2e8595b359c22e4f32b1b
조인영 (In-Young Cho) <ciy405x@kaist.ac.kr>
2018-11-15 01:45:25 +0900

    test button for alarm design

--- app/src/main/java/com/example/cs408_app/MainActivity.java
+++ app/src/main/java/com/example/cs408_app/MainActivity.java
@@ -1,7 +1,14 @@
 package com.example.cs408_app;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.BitmapFactory;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,6 +17,10 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.cs408_app.Config.Constants;
+import com.example.cs408_app.MyFirebaseMessagingService.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -72,6 +83,19 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        /**
+         * Test code for push alarm design
+         */
+        button = findViewById(R.id.button_test_self_alarm);
+        button.setOnClickListener(new Button.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Map<String, String> dataMap = new HashMap<>();
+                dataMap.put("title", "Math is everywhere!!");
+                dataMap.put("content", "Why is the mathematics is the language of science??");
+                sendNotification(dataMap);
+            }
+        });
 
         Boolean is_registered = preferences.getBoolean("is_registered",false);
         if(!is_registered){
@@ -99,5 +123,29 @@ public class MainActivity extends AppCompatActivity {
         TextView tv = findViewById(R.id.text_user);
         String user_email = preferences.getString("user_email", "NO USER EMAIL");
         tv.setText("Current user: "+ user_email);
+    }
+
+    /**
+     * Test code for push alarm design
+     */
+    private void sendNotification(Map<String, String> dataMap){
+
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
+                PendingIntent.FLAG_CANCEL_CURRENT);
+
+        Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this, "MY_channel")
+                .setLargeIcon(BitmapFactory.decodeResource(getResources(), android.R.drawable.ic_dialog_info))
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentTitle(dataMap.get("title"))
+                .setContentText(dataMap.get("content"))
+                .setAutoCancel(true)
+                .setSound(defaultSoundUri)
+                .setContentIntent(pendingIntent);
+
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(0 /* ID of notification */, notificationBuilder.build());
     }
 }
