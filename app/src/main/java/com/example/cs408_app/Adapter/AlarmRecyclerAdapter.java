64af060ad7de45fe422b1d4e4862dc7e0d31f99c
조인영 (In-Young Cho) <ciy405x@kaist.ac.kr>
2018-11-19 19:06:59 +0900

    RecyclerBugFixed+DescendingOrder+ScrollRefresh+Refactoring
    okey dokey

--- app/src/main/java/com/example/cs408_app/Adapter/AlarmRecyclerAdapter.java
+++ app/src/main/java/com/example/cs408_app/Adapter/AlarmRecyclerAdapter.java
@@ -2,16 +2,14 @@ package com.example.cs408_app.Adapter;
 
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.example.cs408_app.AlarmViewActivity;
-import com.example.cs408_app.AllAlarmsActivity;
-import com.example.cs408_app.MainActivity;
 import com.example.cs408_app.Model.AlarmElement;
 import com.example.cs408_app.R;
 
@@ -48,8 +46,25 @@ public class AlarmRecyclerAdapter extends RecyclerView.Adapter<AlarmRecyclerAdap
         public TextView title, desc;
         public ViewHolder(View itemView){
             super(itemView);
-            title = (TextView) itemView.findViewById(R.id.text_title);
-            desc = (TextView) itemView.findViewById(R.id.text_desc);
+            title = itemView.findViewById(R.id.text_title);
+            desc = itemView.findViewById(R.id.text_desc);
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    int position = getAdapterPosition();
+                    if (position != RecyclerView.NO_POSITION) {
+                        AlarmElement selected = item.get(position);
+
+                        Intent intent = new Intent(context, AlarmViewActivity.class);
+
+                        Bundle args = new Bundle();
+                        args.putSerializable("alarm", selected);
+                        intent.putExtras(args);
+                        context.startActivity(intent);
+                    }
+                }
+            });
         }
 
 
--- app/src/main/java/com/example/cs408_app/AllAlarmsActivity.java
+++ app/src/main/java/com/example/cs408_app/AllAlarmsActivity.java
@@ -1,9 +1,8 @@
 package com.example.cs408_app;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -15,13 +14,17 @@ import android.widget.Toast;
 
 import com.example.cs408_app.API.CS4CSApi;
 import com.example.cs408_app.Adapter.AlarmRecyclerAdapter;
-import com.example.cs408_app.Adapter.RecyclerItemClickListener;
 import com.example.cs408_app.Config.Constants;
 import com.example.cs408_app.Model.AlarmElement;
 
-import org.w3c.dom.Text;
-
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -36,44 +39,53 @@ public class AllAlarmsActivity extends AppCompatActivity {
     CS4CSApi apiService;
     TextView textView;
 
-    private LinearLayoutManager layoutManager;
     List<AlarmElement> alarmList = null;
-
+    private RecyclerView recycler;
+    AlarmRecyclerAdapter adapter;
+    private SwipeRefreshLayout swipeContainer;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_all_alarms);
 
+        getAlarmList();
+
+        recycler = findViewById(R.id.recycler);
+
+        // Explicit Refreshing
         Button refresh_btn = findViewById(R.id.refresh_button);
         refresh_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 getAlarmList();
+                Toast.makeText(AllAlarmsActivity.this, "Alarm List Refreshed", Toast.LENGTH_SHORT).show();
             }
         });
-        getAlarmList();
-        RecyclerView recycler = (RecyclerView)findViewById(R.id.recycler);
-        recycler.addOnItemTouchListener(
-                new RecyclerItemClickListener(AllAlarmsActivity.this, recycler, new RecyclerItemClickListener.OnItemClickListener() {
-                    @Override
-                    public void onItemClick(View view, int position) {
-                        Intent intent = new Intent(AllAlarmsActivity.this, AlarmViewActivity.class);
-                        AlarmElement selected = alarmList.get(position);
-                        Bundle args = new Bundle();
-                        args.putSerializable("alarm", selected);
-                        intent.putExtras(args);
-                        startActivity(intent);
-                    }
-
-                    @Override
-                    public void onLongItemClick(View view, int position) {
-                        // nothing much currently
-                    }
-                }));
     }
 
     private void getAlarmList(){
+
+        // Setting Recycler View, Swipe Refresh, Default Layout and Empty Adapter
+        recycler = findViewById(R.id.recycler);
+        alarmList = new ArrayList<>();
+        adapter = new AlarmRecyclerAdapter(AllAlarmsActivity.this, alarmList);
+
+        recycler.setLayoutManager(new LinearLayoutManager(AllAlarmsActivity.this));
+        recycler.setAdapter(adapter);
+        adapter.notifyDataSetChanged(); // any View reflecting the data has been changed should refresh itself
+
+        swipeContainer = findViewById(R.id.swipeRefreshLayout);
+        swipeContainer.setColorSchemeColors(getResources().getColor(android.R.color.holo_green_light));
+        swipeContainer.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                getAlarmList();
+                Toast.makeText(AllAlarmsActivity.this, "Alarm List Refreshed", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        // Get AlarmList from the server
         try{
             retrofit = new Retrofit.Builder()
                     .addConverterFactory(GsonConverterFactory.create()).baseUrl(Constants.server_ip + Constants.server_port).build();
@@ -83,21 +95,42 @@ public class AllAlarmsActivity extends AppCompatActivity {
                 @Override
                 public void onResponse(Call<List<AlarmElement>> call, Response<List<AlarmElement>> response) {
                     alarmList = response.body();
-                    RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recycler);
-                    layoutManager = new LinearLayoutManager(AllAlarmsActivity.this);
-                    recyclerView.setLayoutManager(layoutManager);
-                    AlarmRecyclerAdapter alarmRVA = new AlarmRecyclerAdapter(getApplicationContext(), alarmList);
-                    recyclerView.setAdapter(alarmRVA);
+                    Collections.sort(alarmList, new sortByCreatedAt());
+                    recycler.setAdapter(new AlarmRecyclerAdapter(getApplicationContext(), alarmList));
+                    recycler.smoothScrollToPosition(0);
+                    if (swipeContainer.isRefreshing()){
+                        swipeContainer.setRefreshing(false);
+                    }
                 }
 
                 @Override
                 public void onFailure(Call<List<AlarmElement>> call, Throwable t) {
                     Log.d("AllAlarms","failed to get alarm list");
+                    Toast.makeText(AllAlarmsActivity.this, "Error Fetching Data!", Toast.LENGTH_SHORT).show();
                 }
             });
         }
         catch(Exception e){
             Log.d("AllAlarms", "failed to get alarm list");
+        }
+    }
+
+    class sortByCreatedAt implements Comparator<AlarmElement> {
+        @Override
+        public int compare(AlarmElement a, AlarmElement b) {
+            Date a_date, b_date;
+
+            SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS", Locale.KOREA);
+
+            try {
+                a_date = format.parse(a.getCreated_at());
+                b_date = format.parse(b.getCreated_at());
+                return (a_date.getTime() > b_date.getTime()) ? -1 : (a_date.getTime() < b_date.getTime()) ? 1 : 0;
+            } catch (ParseException e) {
+                e.printStackTrace();
+                return 0;
+            }
+
         }
     }
 }
