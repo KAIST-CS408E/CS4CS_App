93ce5ba2cfde88775c96b85a40f1c7d66cfa9b7f
조인영 (In-Young Cho) <ciy405x@kaist.ac.kr>
2018-11-21 22:19:25 +0900

    toolbar fixed

--- app/src/main/java/com/example/cs408_app/MainActivity.java
+++ app/src/main/java/com/example/cs408_app/MainActivity.java
@@ -19,6 +19,7 @@ import android.support.design.widget.Snackbar;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
